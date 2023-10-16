ab6d90eddac5b1f983f5b51a30792582266c443e
hugoortiz94 <hortiz1@miumg.edu.gt>
2023-10-16 00:30:42 -0600

    Mensaje descriptivo de la confirmación

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
 from flask import Flask
 from flask import render_template
+from PIL import Image
 
 app=Flask(__name__)
+app.static_folder = 'static'
 
 @app.route('/')
 def inicio():
     return render_template('login/login.html')
 
 @app.route('/home')
-def inicio():
+def inicio_home():
     return render_template('home/home.html')
 
 @app.route('/category')
-def inicio():
+def inicio_category():
     return render_template('category/category.html')
 
 @app.route('/details')
-def inicio():
+def inicio_details():
     return render_template('details/details.html')
 
 @app.route('/elements')
-def inicio():
+def inicio_elements():
     return render_template('elements/elements.html')
 
 @app.route('/product_details')
-def inicio():
+def inicio_product_details():
     return render_template('product_details/product_details.html')
 
 @app.route('/signin')
-def inicio():
+def inicio_signin():
     return render_template('signin/signin.html')
-
 
 if __name__ =='__main__':
     app.run(debug=True)
+    
+# Configurar Pillow para manejar imágenes PNG
+Image.warnings.simplefilter('error', Image.DecompressionBombWarning)
